3d8707071eeb3dcefe22a971120f28f9423ec8dd
steso <sergio.teso@nommon.es>
2023-11-08 13:42:02 +0000

    feat: :hammer: Added development environment using docker
    Added a dev environment with docker and sample data with a hello_world app

---
+++ .github/ISSUE_TEMPLATE/feature_request.md
@@ -0,0 +1,17 @@
+---
+name: User story request
+about: Suggest an idea for this project
+title: ''
+labels: ''
+assignees: ''
+
+---
+
+**Describe User story.**
+Brief description of the user story.
+
+**Confluence documentation link**
+Reference to use case of methodology in Confluence.
+
+**Additional context**
+Add any other context or screenshots about the User Story request here.
---
+++ src/test.py
@@ -0,0 +1,41 @@
+import pyspark.sql.functions as psf
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StringType, StructType
+
+class SESSION_TYPES:
+    LOCAL_SESSION = "local"
+    STANDALONE_SESSION = "standalone"
+    YARN_SESSION = "yarn"
+
+def build_local_session():
+    return SparkSession.builder\
+        .config("spark.driver.host", "localhost")\
+        .config("spark.eventLog.enabled", "true")\
+        .config("spark.eventLog.dir", "/opt/spark/spark-events")\
+        .config("spark.history.fs.logDirectory", "/opt/spark/spark-events")\
+            .appName("Test-multimno-submit").getOrCreate()
+
+build_session = {SESSION_TYPES.LOCAL_SESSION: build_local_session}
+
+if __name__=="__main__":
+    SESSION = SESSION_TYPES.LOCAL_SESSION
+
+    spark = build_session[SESSION]()
+
+    sc = spark.sparkContext.getOrCreate()
+
+    input_path = "/opt/data/input/od_pairs.txt"
+    output_path = "/opt/data/output/od_pairs.parquet"
+
+
+    schema = StructType() \
+        .add("origin",StringType(),True) \
+        .add("destination",StringType(),True)
+        
+    df = spark.read.option("delimiter", "|").option("header", False).schema(schema).csv(input_path)
+
+    df = df.withColumn('test', psf.lit('patata'))
+
+    df.write.mode('overwrite').parquet(output_path)
+    df = spark.read.parquet(output_path)
+    df.show()
