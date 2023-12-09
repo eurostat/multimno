778f12a0d74fbbe5a03fab56b59b2403f475da56
Nikolay Kozlovskiy <nikolajkozlovskij73@gmail.com>
2023-12-09 02:05:50 +0200

    feat: added functions to transfrom stage in EventCleaning from notebook

---
+++ src/core/columns.py
@@ -0,0 +1,9 @@
+class ColNames:
+    user_id = 'user_id'
+    timestamp = 'timestamp'
+    cell_id = 'cell_id'
+    longitude = 'longitude'
+    latitude = 'latitude'
+    year = 'year'
+    month = 'month'
+    day = 'day'
