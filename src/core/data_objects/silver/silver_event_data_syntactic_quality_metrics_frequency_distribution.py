cffa48e7fd267e260afb9e93f3e6ff7dcee6c933
Villem Tõnisson <villem.tonisson@positium.com>
2023-12-11 08:41:58 +0000

    feat: Created syntacic quality metrics data objects for events

---
+++ src/core/data_objects/silver/silver_event_data_syntactic_quality_metrics_frequency_distribution.py
@@ -0,0 +1,29 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, TimestampType, FloatType, BinaryType, IntegerType, DateType
+
+from core.data_objects.data_object import PathDataObject
+from core.io_interface import ParquetInterface
+
+
+class SilverEventDataSyntacticQualityMetricsFrequencyDistribution(PathDataObject):
+    ID = "SilverEventDataSyntacticQualityMetricsFrequencyDistribution"
+    SCHEMA = StructType([
+        StructField("cell_id", StringType(), nullable=False),
+        StructField("user_id", BinaryType(), nullable=False),
+        StructField("date", DateType(), nullable=False),
+        StructField("initial_frequency", IntegerType(), nullable=False),
+        StructField("final_frequency", StringType(), nullable=False)
+    ])
+
+    def __init__(self, spark: SparkSession, default_path: str) -> None:
+        super().__init__(spark, default_path)
+        self.interface: ParquetInterface = ParquetInterface()
+        self.partition_columns = None
+
+    def write(self, path: str = None, partition_columns: list[str] = None):
+        if path is None:
+            path = self.default_path
+        if partition_columns is None:
+            partition_columns = self.partition_columns
+
+        self.interface.write_from_interface(self.df, path, partition_columns)
